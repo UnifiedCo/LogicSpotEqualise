a6a8d5069abf17a5460e8ef7551ad55ff996d936
Simon Bowler <simon@logicspot.com>
2014-05-02 14:31:38 +0100

    basic plugin with callbacks and options

--- js/jquery.logicspot-equalise.js
+++ js/jquery.logicspot-equalise.js
@@ -2,11 +2,32 @@
 // Making a self enclosed javascript pattern - ou variables will not interfeer with any other jQuery libraries
 (function($) {
 
-    $.fn.helloWorld = function() {
+    $.fn.helloWorld = function(options) {
 
-        return this.each( function() {
-            $(this).text("Hello, World!");
-        });
+        // Establish our default settings
+        var settings = $.extend({
+            text         : 'Hello, World!',
+            color        : null,
+            fontStyle    : null,
+            complete 	 : null
+        }, options);
+
+		return this.each( function() {
+		    $(this).text( settings.text );
+
+		    if ( settings.color ) {
+		        $(this).css( 'color', settings.color );
+		    }
+
+		    if ( settings.fontStyle ) {
+		        $(this).css( 'font-style', settings.fontStyle );
+		    }
+
+		    // Callback when plugin is executed
+		    if ( $.isFunction( settings.complete ) ) {
+        		settings.complete.call( this );
+    		}
+		});
 
     }
 
